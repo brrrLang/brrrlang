f3ab85689d0c8bb8fd555c24b8def3f8b42f9265
Nathan Hare <nathanhare32@gmail.com>
2020-05-31 21:03:34 +1200

    much brrr

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -1,7 +1,23 @@
+use crate::tree::module::Module;
+
 pub mod module;
 
 pub struct Tree {
 	name: String,
 	root_module: module::Module,
 	children: Vec<module::Module>
+}
+
+impl Tree {
+	pub fn new_no_children(name: String, root_module: module::Module) -> Tree {
+		Tree { name, root_module, children: vec!() }
+	}
+
+	pub fn new(name: String, root_module: module::Module, children: Vec<module::Module>) -> Tree {
+		Tree { name, root_module, children }
+	}
+
+	pub fn has_children(&self) -> bool {
+		return self.children.is_empty();
+	}
 }
--- src/tree/module.rs
+++ src/tree/module.rs
@@ -5,3 +5,19 @@ pub struct Module {
 	pub sub_modules: Vec<Module>,
 	pub code: Vec<Box<dyn syntax_tree::SyntaxTreeElement>>
 }
+
+impl Module {
+	pub fn new(module_name: &String, sub_modules: &Vec<Module>, code: &Vec<Box<dyn syntax_tree::SyntaxTreeElement>>) -> Module {
+		let module_name = format!("{}", module_name);
+		let sub_modules: Vec<Module> = sub_modules.to_vec();
+		let code: Vec<Box<dyn syntax_tree::SyntaxTreeElement>> = code.to_vec();
+		Module { module_name, sub_modules, code }
+	}
+
+	pub fn new_no_subs(module_name: &String, code: &Vec<Box<dyn syntax_tree::SyntaxTreeElement>>) -> Module {
+		let module_name = format!("{}", module_name);
+		let code: Vec<Box<dyn syntax_tree::SyntaxTreeElement>> = code.to_vec();
+		let sub_modules = vec!();
+		Module { module_name, code, sub_modules }
+	}
+}
