38d6a5135369a3e9010f9051029aeeb63898b434
Nathan Hare <me@laspruca.nz>
2020-11-22 16:37:02 +1300

    Added some thread safety to error/warning reporters

--- src/error_handler/error.rs
+++ src/error_handler/error.rs
@@ -1,34 +1,41 @@
-use ansi_term::Colour;
+use std::io::prelude::*;
 use std::process;
+
+use ansi_term::Colour;
 
 use crate::error_handler::*;
 
 const STYLE: Colour = Colour::Red;
 
 pub fn error_reporter(error: Error) {
-	println!(
-		"{}{}",
-		STYLE.bold().paint(format!("\n{} error", error.error_area)),
-		STYLE.paint(format!(": {}",error.message)
-	));
-	if error.line_num != -1 {
-		println!(
-			"{}{}",
-			STYLE.paint(format!(
-				"Line {} {}",
-				error.line_num,
-				if error.line_text != String::new() {
-					String::from("--> ")
-				}
-				else {
-					String::new()
-				}
-			)),
-			Colour::White.italic().paint(error.line_text)
-		);
-	} else if error.message != String::new() {
-		println!("{}{}",STYLE.paint("Line: "),Colour::White.italic().paint(error.line_text))
-	}
-	println!();
-	process::exit(-1);
+    let out = std::io::stdout();
+    let mut lock = out.lock();
+    writeln!(
+        &mut lock,
+        "{}{}",
+        STYLE.bold().paint(format!("\n{} error", error.error_area)),
+        STYLE.paint(format!(": {}", error.message)
+        ));
+    if error.line_num != -1 {
+        writeln!(
+            &mut lock,
+            "{}{}",
+            STYLE.paint(format!(
+                "Line {} {}",
+                error.line_num,
+                if error.line_text != String::new() {
+                    String::from("--> ")
+                } else {
+                    String::new()
+                }
+            )),
+            Colour::White.italic().paint(error.line_text)
+        );
+    } else if error.message != String::new() {
+        writeln!(&mut lock, "{}{}", STYLE.paint("Line: "), Colour::White.italic().paint(error.line_text));
+    }
+    writeln!(&mut lock);
+    drop(lock);
+
+    process::exit(-1);
 }
--- src/error_handler/warning.rs
+++ src/error_handler/warning.rs
@@ -1,3 +1,5 @@
+use std::io::prelude::*;
+
 use ansi_term::Colour;
 
 use crate::error_handler::*;
@@ -5,28 +7,32 @@ use crate::error_handler::*;
 const STYLE: Colour = Colour::Yellow;
 
 pub fn warning_reporter(warning: Warning) {
-	println!(
-		"{}{}",
-		STYLE.bold().paint(format!("\n{} warning", warning.error_area)),
-		STYLE.paint(format!(": {}",warning.message)
-	));
-	if warning.line_num != -1 {
-		println!(
-			"{}{}",
-			STYLE.paint(format!(
-				"Line {} {}",
-				warning.line_num,
-				if warning.line_text != String::new() {
-					String::from("")
-				}
-				else {
-					String::new()
-				}
-			)),
-			Colour::White.italic().paint(warning.line_text)
-		);
-	} else if warning.message != String::new() {
-		println!("{}{}",STYLE.paint("Line: "),Colour::White.italic().paint(warning.line_text))
-	}
-	println!();
+    let out = std::io::stdout();
+    let mut lock = out.lock();
+    writeln!(
+        &mut lock,
+        "{}{}",
+        STYLE.bold().paint(format!("\n{} warning", warning.error_area)),
+        STYLE.paint(format!(": {}", warning.message)
+        ));
+    if warning.line_num != -1 {
+        writeln!(
+            &mut lock,
+            "{}{}",
+            STYLE.paint(format!(
+                "Line {} {}",
+                warning.line_num,
+                if warning.line_text != String::new() {
+                    String::from("")
+                } else {
+                    String::new()
+                }
+            )),
+            Colour::White.italic().paint(warning.line_text)
+        );
+    } else if warning.message != String::new() {
+        writeln!(&mut lock, "{}{}", STYLE.paint("Line: "), Colour::White.italic().paint(warning.line_text));
+    }
+    writeln!(&mut lock);
+    drop(lock);
 }
